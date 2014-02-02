03a8dd28ec86f39d01540b7ded02e6ef168930dd
Ryan <hiphubryan@gmail.com>
2014-02-02 22:05:34 +0800

    initial bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingHouse::Application.config.secret_key_base = '0bf4c7da7f478ae8829dddb17ced1b498009f5e6bd2c14abe082e49a8332b1a0e0cf36be185d635bcf82944a18f0dd6b18d43f587e0b41aa2f8013a8c7cece23'
